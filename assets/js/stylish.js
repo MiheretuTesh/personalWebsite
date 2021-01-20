6882020943e653c3391b17c56b3af73021c26385
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-01-20 23:01:55 +0300

    The pahes responsiveness is partially done.

--- assets/js/stylish.js
+++ assets/js/stylish.js
@@ -9,3 +9,29 @@ $(".btn-close").click(function(e){
     $(".navbar-collapse").removeClass("show");
     $("body").removeClass("offcanvas-active");
 });
+
+
+
+// $( ".fa-index" ).mouseover(function()
+// {
+//     $(this).addClass("fa-times");
+// $(this).removeClass("fa-angle-down");
+// });
+
+// $( ".fa-index" ).mouseout(function()
+// {
+//     $(this).removeClass("fa-times");
+//     $(this).addClass("fa-angle-down");
+
+// });
+
+
+// $(function(){   
+//     $(".fa-index").hover(     
+//     function () {       
+//     $(this).toggleClass('fa-times fa-angle-down hovered');
+//     },
+//           function ()
+//     {       
+//     $(this).toggleClass('fa-times fa-angle-down hovered');     
+//     }    );});
